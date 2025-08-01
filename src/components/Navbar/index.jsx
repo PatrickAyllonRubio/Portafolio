f84f5eaa3fc6b05269fddf5c4061814f2171125f
Patrick <patrick.ayllon@tecsup.edu.pe>
2025-08-01 15:18:36 -0500

    Version final 1.3

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import "./Navbar.css";
-import Logo from "../../assets/logo";
+import Logo from "../../assets/Logo";
 import { FaTimes } from "react-icons/fa";
 import { menu } from "../../data";
 import { Link, animateScroll as scroll } from "react-scroll";
